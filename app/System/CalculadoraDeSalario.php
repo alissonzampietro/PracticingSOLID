8ff441dbe7bf71d54da592e1fc6ba78cf89e9cef
Alisson Zampietro <alisson.zampietro@gmail.com>
2017-04-09 22:17:17 -0300

    Implementando tipos de descontos

--- app/System/CalculadoraDeSalario.php
+++ app/System/CalculadoraDeSalario.php
@@ -8,6 +8,28 @@ class CalculadoraDeSalario
 
     public function calcula(Funcionario $funcionario)
     {
-        switch ($funcionario->getCargo())
+        $salarioLiquido = $funcionario->getSalario();
+        switch (strtoupper($funcionario->getCargo())){
+            case "Programador":
+                $salarioLiquido = $this->dezOuVintePorcento($funcionario);
+                break;
+            case "Segurança":
+                $salarioLiquido = $this->quinzeOuVinteCincoPorcento($funcionario);
+                break;
+            default:
+                return FALSE;
+                break;
+               
+        }
+    }
+    
+    private function dezOuVintePorcento(Funcionario $funcionario)
+    {
+        return $funcionario->getSalario() - ($funcionario->getSalario() * 0.1); 
+    }
+    
+    private function quinzeOuVinteCincoPorcento(Funcionario $funcionario)
+    {
+        return $funcionario->getSalario() - ($funcionario->getSalario() * 0.25);
     }
 }
--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,3 +1,12 @@
 <?php
 
-use
+use App\System\CalculadoraDeSalario;
+use App\System\Funcionario;
+
+$calc = new CalculadoraDeSalario();
+$func = new Funcionario();
+
+$func->setcargo("Programador");
+$func->setSalario(3000);
+
+$calc->calcula($func);
